78d90ae243f1713793483e7a2e33606aaecf1d89
DinoPy <al3x.dinoiu@gmail.com>
2022-08-13 22:30:02 +0300

    commented code in products page as serve json is not available.

--- pages/product/[productId].jsx
+++ pages/product/[productId].jsx
@@ -1,45 +1,45 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React from 'react';
+import { useRouter } from 'next/router';
 
 function Product({ product }) {
-  const router = useRouter();
+	const router = useRouter();
 
-  if (router.isFallback) {
-    return <div> Loading... </div>;
-  }
+	if (router.isFallback) {
+		return <div> Loading... </div>;
+	}
 
-  return (
-    <div>
-      <h2>
+	return (
+		<div>
+			{/* <h2>
         {product.id} {product.title} {product.price}
       </h2>
       <p> {product.description} </p>
-      <hr />
-    </div>
-  );
+      <hr /> */}
+		</div>
+	);
 }
 
 export default Product;
 
-export async function getStaticProps(context) {
-  const { params } = context;
+// export async function getStaticProps(context) {
+//   const { params } = context;
 
-  const response = await fetch(
-    `http://localhost:4000/products/${params.productId}`
-  );
-  const data = await response.json();
+//   const response = await fetch(
+//     `http://localhost:4000/products/${params.productId}`
+//   );
+//   const data = await response.json();
 
-  return {
-    props: {
-      product: data,
-    },
-    revalidate: 10,
-  };
-}
+//   return {
+//     props: {
+//       product: data,
+//     },
+//     revalidate: 10,
+//   };
+// }
 
-export async function getStaticPaths() {
-  return {
-    paths: [{ params: { productId: "1" } }],
-    fallback: true,
-  };
-}
+// export async function getStaticPaths() {
+//   return {
+//     paths: [{ params: { productId: "1" } }],
+//     fallback: true,
+//   };
+// }
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -4,7 +4,7 @@ function ProductList({ products }) {
 	return (
 		<>
 			<h1> List of products</h1>
-			{products.map((product) => {
+			{/* {products.map((product) => {
 				return (
 					<div key={product.id}>
 						<h2>
@@ -13,22 +13,22 @@ function ProductList({ products }) {
 						</h2>
 					</div>
 				);
-			})}
+			})} */}
 		</>
 	);
 }
 
 export default ProductList;
 
-export async function getStaticProps() {
-	console.log('generating or regenerating product list');
-	const response = await fetch('http://localhost:4000/products');
-	const data = await response.json();
+// export async function getStaticProps() {
+// 	console.log('generating or regenerating product list');
+// 	const response = await fetch('http://localhost:4000/products');
+// 	const data = await response.json();
 
-	return {
-		props: {
-			products: data,
-		},
-		revalidate: 10,
-	};
-}
+// 	return {
+// 		props: {
+// 			products: data,
+// 		},
+// 		revalidate: 10,
+// 	};
+// }
